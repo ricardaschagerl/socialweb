b3db984920e29f5b404ad958d7ad98a71ad5f212
sendl <endl.stefan@gmail.com>
2017-07-06 21:32:20 +0200

    Second Page with Friends created

--- js/API/facebook.js
+++ js/API/facebook.js
@@ -1,32 +1,135 @@
-$(document).ready(function () {
+var $user;
 
-    function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
+function checkLoginState(response) {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+}
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    $loginButton = $(".fb-login-button").detach();
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        console.log("Logged in");
+        $loginButton.appendTo("#media-buttons");
+        showMe();
+    } else {
+        // The person is not logged into your app or we are unable to tell.
+        console.log("Please Log in");
+        $loginButton.appendTo("#jumbo");
+        deleteMe();
+    }
+}
+
+function deleteMe() {
+    console.log("Logged out - delete user content.")
+    if ($("#jumbo").length) {
+        $(".media-object.me").attr("src", "");
+        $(".media-heading.me").text("");
+        $(".media-body.me").text("");
+        $("#userdata").hide();
+    } else {
+        window.location.replace("index.html");
+    }
+}
+
+function showMe() {
+    console.log("Loading user data...");
+    FB.api(
+        '/me',
+        'GET',
+        {"fields": "id,name,hometown,location"},
+        function (response) {
+            if (response != null) {
+                console.log("User data found!");
+                console.log(response);
+
+                currentUser = response;
+                $user = currentUser;
+
+                if(typeof window.friendFlag !== 'undefined'){
+                    showFriends();
+                }
+
+                $(".media-object.me").attr("src", "http://graph.facebook.com/" + currentUser.id + "/picture/");
+                $(".media-heading.me").text(currentUser.name);
+                var text = "";
+                if (currentUser.location && currentUser.location.name) {
+                    text = "von " + currentUser.location.name + ".";
+                } else {
+                    if (currentUser.hometown && currentUser.hometown.name) {
+                        text = "von " + currentUser.hometown.name + ".";
+                    } else {
+                        text = "Kein Ort angegeben.";
+                    }
+                }
+                $(".media-body.me").append(text);
+                $("#userdata").show();
+                console.log("User data loaded.");
+            }
+        }
+    );
+}
+
+function showLikes(user) {
+    if (user) {
+        let html = user.name + " likes: </br>";
+        FB.api("/" + user.id + "/likes", (likes) => {
+            console.log(likes);
+            if (likes && likes.data) {
+                for (var i = 0; i < likes.data.length; i++) {
+                    html += likes.data[i].name + " - ";
+                }
+                $likes = $("#likes");
+                $likes.html(html);
+                $likes.show();
+            }
         });
     }
+}
 
-    function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
-        $loginButton = $(".fb-login-button").detach();
-        if (response.status === 'connected') {
-            // Logged into your app and Facebook.
-            console.log("Logged in");
-            $loginButton.appendTo("#media-buttons");
-            showMe();
-        } else {
-            // The person is not logged into your app or we are unable to tell.
-            console.log("Please Log in");
-            $loginButton.appendTo("#jumbo");
-            deleteMe();
+
+function showLocation() {
+    if (hometown != null && hometown.name != null && currentUser != null) {
+        var geocoder = new google.maps.Geocoder();
+        if (geocoder) {
+            geocoder.geocode(
+                {address: hometown.name},
+                function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        userLatLong = results[0].geometry.location;
+                        map.setCenter(userLatLong);
+                        /*if (marker != null){
+                         marker.setMap(null);
+                         }    */
+                        marker = new google.maps.Marker({
+                            position: userLatLong,
+                            map: map,
+                            icon: {url: 'home.gif'},
+                            title: currentUser.name
+                        });
+                        //calculateAndDisplayRoute(curLatLong,userLatLong)
+                        // google.maps.event.addListener(marker, "click", function() {
+                        //     createInfoWindow(currentUser,marker);
+                        //  });
+                        calculateAndDisplayRoute(userLatLong, userLatLong)
+                    } else {
+                        alert(address + " was not found.");
+                    }
+                });
         }
+    } else {
+        alert("User does not provide hometown");
     }
+}
 
+$(document).ready(function () {
     var loginLogoutEvent = statusChangeCallback;
 
     $.ajaxSetup({cache: true});
@@ -43,115 +146,5 @@ $(document).ready(function () {
         checkLoginState();
     });
 
-    function deleteMe(){
-        console.log("Logged out - delete user content.")
-        $(".media-object").attr("src", "");
-        $(".media-heading").text("");
-        $(".media-body").text("");
-        $("#userdata").hide();
-    }
-
-    function showMe() {
-        console.log("Now logged in - should load user data");
-        FB.api(
-            '/me',
-            'GET',
-            {"fields": "id,name,hometown,likes,friends,location"},
-            function (response) {
-                if (response != null) {
-                    console.log(response);
-                    currentUser = response;
-
-                    $(".media-object").attr("src", "http://graph.facebook.com/" + currentUser.id + "/picture/");
-                    $(".media-heading").text(currentUser.name);
-                    var text = "";
-                    if(currentUser.location && currentUser.location.name){
-                        text = "von " +  currentUser.location.name + ".";
-                    } else {
-                        if (currentUser.hometown && currentUser.hometown.name) {
-                            text = "von " + currentUser.hometown.name + ".";
-                        } else {
-                            text = "Kein Ort angegeben.";
-                        }
-                    }
-                    $(".media-body").append(text);
-                    $("#userdata").show();
-                }
-            }
-        );
-    }
-
-    function showLikes(user) {
-        if (user) {
-            let html = user.name + " likes: </br>";
-            FB.api("/" + user.id + "/likes", (likes) => {
-                console.log(likes);
-                if (likes && likes.data) {
-                    for (var i = 0; i < likes.data.length; i++) {
-                        html += likes.data[i].name + " - ";
-                    }
-                    $likes = $("#likes");
-                    $likes.html(html);
-                    $likes.show();
-                }
-            });
-        }
-    }
-
-    function showFriends(user) {
-        if (user) {
-            let html = user.name + "s Freunde: </br>";
-            FB.api("/" + user.id + "/friends", "GET", {"fields": "id,name,friends,likes"}, (friends) => {
-                console.log(friends.data[0].likes.data);
-                if (friends && friends.data) {
-                    for (var i = 0; i < friends.data.length; i++) {
-                        html += friends.data[i].name;
-                        html += friends.data[i].id;
-                        var likes = friends.data[i].likes.data;
-                        for (var i = 0; i < likes.length; i++) {
-                            html += likes[i].name + " - ";
-                        }
-
-                    }
-                    $friends = $("#friends");
-                    $friends.html(html);
-                    $friends.show();
-                }
-            });
-        }
-    }
-
-    function showLocation() {
-        if (hometown != null && hometown.name != null && currentUser != null) {
-            var geocoder = new google.maps.Geocoder();
-            if (geocoder) {
-                geocoder.geocode(
-                    {address: hometown.name},
-                    function (results, status) {
-                        if (status == google.maps.GeocoderStatus.OK) {
-                            userLatLong = results[0].geometry.location;
-                            map.setCenter(userLatLong);
-                            /*if (marker != null){
-                             marker.setMap(null);
-                             }    */
-                            marker = new google.maps.Marker({
-                                position: userLatLong,
-                                map: map,
-                                icon: {url: 'home.gif'},
-                                title: currentUser.name
-                            });
-                            //calculateAndDisplayRoute(curLatLong,userLatLong)
-                            // google.maps.event.addListener(marker, "click", function() {
-                            //     createInfoWindow(currentUser,marker);
-                            //  });
-                            calculateAndDisplayRoute(userLatLong, userLatLong)
-                        } else {
-                            alert(address + " was not found.");
-                        }
-                    });
-            }
-        } else {
-            alert("User does not provide hometown");
-        }
-    }
 });
+
--- js/friends.js
+++ js/friends.js
@@ -1,7 +1,49 @@
-//Corinna is here
-$(document).ready(function() {
-	console.log("Ready to go");
+var friendFlag = true;
 
+function showFriends() {
+    console.log("Loading friends data...");
+    FB.api("/v2.9/" + $user.id + "/friends", "GET", {"fields": "uid"}, (friends) => {
+        if (friends && friends.data) {
+            console.log("Friends data found!");
+            console.log(friends);
+            for (var i = 0; i < friends.data.length; i++) {
+                addFriend(friends.data[i].name,
+                    friends.data[i].id,
+                    friends.data[i].hometown,
+                    friends.data[i].location);
+            }
+        }
+    });
+    console.log("Friends data loaded.");
+}
 
+function addFriend(name, id, hometown, location){
+    let $text = "";
+    if (location && location.name) {
+        $text = "von " + location.name + ".";
+    } else {
+        if (hometown && hometown.name) {
+            $text = "von " + hometown.name + ".";
+        } else {
+            $text = "Kein Ort angegeben.";
+        }
+    }
 
-});
+    $("#friendContainer").append(
+        '<div class="col-md-6">' +
+            '<div class="panel panel-default">' +
+                '<div class="panel-body">' +
+                    '<div class="media">' +
+                        '<div class="media-left">' +
+                            '<a href="#">' +
+                                '<img class="media-object" src="http://graph.facebook.com/' + id + '/picture/">' +
+                            '</a>' +
+                        '</div>' +
+                        '<div class="media-body">' +
+                            '<h4 class="media-heading">' + name + '</h4>\n' + $text +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>');
+}
